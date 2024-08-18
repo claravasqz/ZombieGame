4d7feae38d8503f73ff550994747e0c02843786d
Claravasqz <claravasquez94@gmail.com>
2024-08-18 11:57:19 -0400

    Almost Done

--- app.js
+++ app.js
@@ -1,132 +1,131 @@
-// 1-.You attack the first alien ship
-// 2-.If the ship survives, it attacks you
-// 3-.If you survive, you attack the ship again
-// 4-.If it survives, it attacks you again ... etc
-// 5-.If you destroy the ship, you have the option to attack the next ship or to retreat
-// 6-.If you retreat, the game is over, perhaps leaving the game open for further developments or options
-// 7-.You win the game if you destroy all of the aliens
-// 8-.You lose the game if you are destroyed
-
-// hull is the same as hitpoints. If hull reaches 0 or less, the ship is destroyed
-// firepower is the amount of damage done to the hull of the target with a successful hit
-// accuracy is the chance between 0 and 1 that the ship will hit its target
-
-
-// Constructing a Ship 
-
-class Ship{
-    constructor (hull, firepower, accuracy) {
+class Player {
+  constructor(hull, firepower, accuracy) {
       this.hull = hull;
       this.firepower = firepower;
       this.accuracy = accuracy;
-    }
-    
-    displayFeatures(){
-      console.log(`Hull: ${this.hull}`)
-      console.log(`firepower: ${this.firepower}`)
-      console.log(`accuracy: ${this.accuracy}`)
-    }
   }
 
-// USSShip
+  attack(currentPlayer) {
+      if (Math.random() < this.accuracy) {
+          const damage = this.firepower;
+          currentPlayer.hull -= damage;
+          console.log(`Hit! ${currentPlayer.constructor.name}'s hull is now ${currentPlayer.hull}`);
+          window.alert(`You hit the Zombie! Zombie's live is now ${currentPlayer.hull}`)
+          return damage;
+      } else {
+          console.log(`${this.constructor.name} missed!`);
+          return 0; 
+      }
+  }
 
-// Your spaceship, the USS Assembly should have the following properties:
-// hull - 20
-// firepower - 5
-// accuracy - .7
+  isAlive() {
+      return this.hull > 0;
+  }
 
-const USSShip = new Ship(20, 5, 0.7);
-USSShip.displayFeatures();
+  displayFeatures() {
+      console.log(`Hull: ${this.hull}`);
+      console.log(`Firepower: ${this.firepower}`);
+      console.log(`Accuracy: ${this.accuracy}`);
+  }
+}
 
-
-
-// Allien Ship
-
-// hull - between 3and 6
-// firepower - between 2and 4
-// accuracy - between .6and .8
 
 function NumRandomRange(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) ) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+let hero, zombies, currentZombieIndex, round;
+
+function initializeGame() {
+  hero = new Player(20, 5, 0.7);
+  zombies = [];
+  for (let i = 0; i < 6; i++) {
+      zombies.push(generateZombie());
   }
+  currentZombieIndex = 0;
+  round = 1;
+
+  document.getElementById('heroHull').textContent = hero.hull;
+  document.getElementById('zombieHull').textContent = zombies[currentZombieIndex].hull;
+  document.getElementById('roundNumber').textContent = round;
+  document.getElementById('heroAttack').disabled = false;
+}
+
+function generateZombie() {
+  const hull = NumRandomRange(3, 6);
+  const firepower = NumRandomRange(2, 4);
+  const accuracy = NumRandomRange(6, 8) / 10;
+  return new Player(hull, firepower, accuracy);
+}
+
+document.getElementById('startGame').addEventListener('click', function() {
+  this.style.display = 'none';
+  document.getElementById('heroAttack').style.display = 'block';
+  document.getElementById('gameStatus').style.display = 'block';
+  document.getElementById('restartGame').style.display = 'none';
+  initializeGame();
+});
+
+document.getElementById('heroAttack').addEventListener('click', function () {
+  let currentZombie = zombies[currentZombieIndex];
   
-// for (let i = 0; i < 6; i++) {
-function generateAlienShip(){
-    const hull = NumRandomRange(3, 6);
-    const firepower = NumRandomRange(2, 4);
-    const accuracy = NumRandomRange(6, 8) / 10;
-    return new Ship(hull, firepower, accuracy);
-}
- // }
+  console.log(`\nRound ${round}:`);
+  
 
-console.log('------')
+  hero.attack(currentZombie);
 
-let AlienShip = [generateAlienShip(), generateAlienShip(), generateAlienShip(), generateAlienShip(), generateAlienShip(), generateAlienShip()];
 
-// const AlienShip1 = generateAlienShip();
-// AlienShip1.displayFeatures();
-    
-// const AlienShip2 = generateAlienShip();
-// AlienShip2.displayFeatures();
-    
-// // const AlienShip3 = generateAlienShip();
-// // AlienShip3.displayFeatures();
+  document.getElementById('zombieHull').textContent = currentZombie.hull;
 
-// // const AlienShip4 = generateAlienShip();
-// // AlienShip4.displayFeatures();
+  if (!currentZombie.isAlive()) {
+      window.alert(`You have defeated zombie ${currentZombieIndex + 1}!`);
+      console.log(`You destroyed zombie ${currentZombieIndex + 1}!`);
+      currentZombieIndex++;
 
-// // const AlienShip5 = generateAlienShip();
-// // AlienShip5.displayFeatures();
-
-// // const AlienShip6 = generateAlienShip();
-// // AlienShip6.displayFeatures();
-
-console.log('------')
-// while(USSShip.hull > 0 && AlienShip1.hull > 0){
-  function USSattack(){
-    if (Math.random() < USSShip.accuracy) {
-      AlienShip.hull -= USSShip.firepower;
-      console.log('You hit the alien ship! yor new life is: ' + AlienShip1.hull);
-
-      if (AlienShip1.hull <= 0){
-        console.log('You destroyed the alien ship!');
-        return;
+      if (currentZombieIndex >= zombies.length) {
+          console.log('You have destroyed all the zombies! You won the game!');
+          window.alert('You have destroyed all the zombies! You won the game!');
+          document.getElementById('heroAttack').disabled = true; 
+          document.getElementById('restartGame').style.display = 'block'; 
+          return; 
       }
-      else{
-        console.log('The alien ship is still alive!')
-        
-      }
-        
-    } else {
-      console.log('You missed the alien ship!');
-    }
-  }
-// }
-    
-USSattack();
 
-
-console.log('------')
-
-function AlienAttack(){
-  if (Math.random() < AlienShip2.accuracy) {
-    USSShip.hull -= AlienShip2.firepower;
-    console.log('You got hit! yor new life is: ' + USSShip.hull);
-
-    if (USSShip.hull <= 0){
-      console.log('You got destroyed');
+      round++;
+      currentZombie = zombies[currentZombieIndex];
+      document.getElementById('zombieHull').textContent = currentZombie.hull;
+      document.getElementById('roundNumber').textContent = round;
       return;
-    }
-    else{
-      console.log('You are still alive')
-        return;
-    }
-      
-  } else {
-    console.log('They missed!');
   }
-}
 
-AlienAttack();
-AlienAttack();
-AlienAttack();
+
+  const damageDealt = currentZombie.attack(hero);
+
+
+  if (damageDealt > 0) {
+      window.alert(`Zombie ${currentZombieIndex + 1} attacked you and dealt ${damageDealt} damage!`);
+  } else {
+      window.alert(`Zombie ${currentZombieIndex + 1} attacked you but missed!`);
+  }
+
+
+  document.getElementById('heroHull').textContent = hero.hull;
+
+  if (!hero.isAlive()) {
+      console.log('Your hero is destroyed! Game over!');
+      window.alert('Your hero is destroyed! Game over!');
+      document.getElementById('heroAttack').disabled = true; 
+      document.getElementById('restartGame').style.display = 'block'; 
+      return; 
+  }
+
+  console.log('Attack again to continue the battle!');
+});
+
+document.getElementById('restartGame').addEventListener('click', function() {
+  initializeGame();
+  this.style.display = 'none';
+  document.getElementById('startGame').style.display = 'block';
+  document.getElementById('heroAttack').style.display = 'none';
+  document.getElementById('gameStatus').style.display = 'none';
+});
